8bba1c2d77a0061f4194e1b3ef86f913a9abd67e
JoelMedrano <jvmedranog@gmail.com>
2018-07-11 14:17:01 -0500

    plantilla

---
+++ config/global.php
@@ -0,0 +1,19 @@
+<?php 
+//Ip de la pc servidor de base de datos
+define("DB_HOST","192.168.1.7");
+
+//Nombre de la base de datos
+define("DB_NAME", "db_corpvasco");
+
+//Usuario de la base de datos
+define("DB_USERNAME", "admin");
+
+//Contraseña del usuario de la base de datos
+define("DB_PASSWORD", "vasco123");
+
+//definimos la codificación de los caracteres
+define("DB_ENCODE","utf8");
+
+//Definimos una constante como nombre del proyecto
+define("PRO_NOMBRE","ITVentas");
+?>
