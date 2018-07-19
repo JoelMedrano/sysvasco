bd34b3b3107416a28e06a27e9fd47887cdb54595
JoelMedrano <jvmedranog@gmail.com>
2018-07-19 15:36:51 -0500

    udp

--- vistas/header.php
+++ vistas/header.php
@@ -252,7 +252,25 @@ if (strlen(session_id()) < 1)
             </li>';
             }
             ?>
-                      
+              
+
+
+            <?php 
+            if ($_SESSION['udp']==1)
+            {
+              echo '<li class="treeview">
+              <a href="#">
+                <i class="fa fa-calendar"></i> <span>U D P</span>
+                <i class="fa fa-angle-left pull-right"></i>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="permiso_personal.php"><i class="fa fa-circle-o"></i> Fichas Tecnicas</a></li>
+           
+           
+              </ul>
+            </li>';
+            }
+            ?>                      
           </ul>
         </section>
         <!-- /.sidebar -->
