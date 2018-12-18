662c8db041f264539a3d8fe71b5c7279f69b1595
JoelMedrano <jvmedranog@gmail.com>
2018-12-18 16:42:40 -0500

    MARCACIONES

---
+++ modelos/Registro_Marcaciones.php
@@ -0,0 +1,199 @@
+<?php 
+//Incluímos inicialmente la conexión a la base de datos
+require "../config/Conexion.php";
+
+Class Registro_Marcaciones
+{
+	//Implementamos nuestro constructor
+	public function __construct()
+	{
+
+	}
+
+
+	//Implementamos un método para actualizar el contrato
+	public function editar($id_trab,
+										   $id_con_trab,
+										   $tie_ren_ant,
+										   $fec_ini_ant,
+										   $fec_fin_ant,
+										   $id_sit_inf_ant,
+										   $usu_reg,
+										   $pc_reg,
+										   $fec_reg  )
+	{
+		
+		$sql="UPDATE contratos SET tie_ren_con='$tie_ren_ant', fec_ini_con='$fec_ini_ant', fec_fin_con='$fec_fin_ant',  id_sit_inf='$id_sit_inf_ant' WHERE id_trab='$id_trab' AND  id_con_trab='$id_con_trab' ";
+		return ejecutarConsulta($sql);
+
+
+	}
+
+	
+    
+
+
+	
+
+
+	public function insertar2(			 $id_trab,
+										 $tie_ren_con,
+										 $fec_ini_con,
+										 $fec_fin_con,
+										 $id_sit_inf_act,
+										 $CantItems,
+										 $usu_reg,
+										 $pc_reg,
+										 $fec_reg )
+	{
+		
+
+
+
+
+	$CantItems=$CantItems+1;
+		
+			$sql= "INSERT INTO contratos  (   		   id_trab,
+													   id_con_trab,
+													   tie_ren_con,
+													   fec_ini_con,
+													   fec_fin_con,
+													   id_sit_inf,
+													   usu_reg,
+													   pc_reg,
+													   fec_reg  ) 
+											   VALUES( '$id_trab',
+											   		   '$CantItems',
+											           '$tie_ren_con',
+											           '$fec_ini_con',
+											           '$fec_fin_con',
+											           '$id_sit_inf_act',
+											           '$usu_reg',
+											           '$pc_reg',
+											           '$fec_reg')  ";
+		
+			
+			
+		
+
+			return ejecutarConsulta($sql);
+
+	}
+
+
+	//Implementamos un método para anular la venta
+	public function anular($nro_doc)
+	{
+		$sql="UPDATE venta SET estado='Anulado' WHERE idventa='$idventa'";
+		return ejecutarConsulta($sql);
+	}
+
+
+	//Implementar un método para mostrar los datos de un registro a modificar
+	public function mostrar($nro_doc)
+	{
+		$sql="SELECT Tra.id_trab, Tra.num_doc_trab AS nro_doc, Tra.num_doc_trab AS id_nomtrab ,  CONCAT(Tra.apepat_trab, ' ' , Tra.apemat_trab, ' ', Tra.nom_trab)   AS apellidosynombres ,   Tra.apemat_trab, Tra.apepat_trab, Tra.nom_trab, Tra.id_sucursal, Tra.id_area, TbAre.Des_Larga AS area_trab,
+              TbSua.des_larga AS sucursal, DATE_FORMAT(fec_ing_trab, '%d/%m/%Y')  AS fec_ing_trab, IFNULL(MAX(con.id_con_trab),0) AS CantItems,
+               MAX(DATE(con.fec_ini_con)) AS  fec_ini_ant , MAX(DATE(con.fec_fin_con)) AS fec_fin_ant, con.tie_ren_con AS tie_ren_ant, con.id_con_trab,
+               con.id_sit_inf AS id_sit_inf_ant,  TbSic.des_larga AS situacion_informativa_actual
+				FROM Trabajador Tra
+				LEFT JOIN contratos con ON
+					con.id_trab= tra.id_trab
+				LEFT JOIN tabla_maestra_detalle TbAre ON
+					TbAre.cod_tabla='TARE'
+					AND TbAre.cod_argumento= Tra.id_area
+				LEFT JOIN tabla_maestra_detalle TbSua ON
+					TbSua.cod_tabla='TSUA'
+					AND TbSua.cod_argumento= Tra.id_sucursal
+				LEFT JOIN tabla_maestra_detalle TbSic ON
+					TbSic.cod_tabla='TSIC'
+					AND TbSic.cod_argumento= con.id_sit_inf
+				WHERE  tra.num_doc_trab='$nro_doc' 
+              ";
+		return ejecutarConsultaSimpleFila($sql);
+	}
+
+	public function listarDetalle($nro_doc)
+	{
+		$sql="SELECT nro_doc, id_periodo, correlativo,TbPea.des_larga AS PeridoAnual, DATE(fec_del) AS  fec_del, DATE(fec_al) AS fec_al, tot_dias, pen_dias, vencidas, truncas, DATE_FORMAT(fec_del_dec, '%d/%m/%Y') AS   fec_del_dec, DATE_FORMAT(fec_al_dec, '%d/%m/%Y') AS  fec_al_dec, tot_dias_dec,
+				 pen_dias_dec, inicio_prog, salida_prog, tot_dias_prog, obser, obser_detalle
+				FROM Vacaciones vac
+				LEFT JOIN tabla_maestra_detalle  TbPea ON
+				TbPea.cod_tabla='TPEA'
+				AND TbPea.cod_argumento= vac.id_periodo
+				where vac.nro_doc='$nro_doc'
+				ORDER BY  vac.correlativo ASC";
+		return ejecutarConsulta($sql);
+	}
+
+	//Implementar un método para listar los registros
+	public function listar($fecha_inicio,$fecha_fin,$id_trab)
+	{
+		$sql="SELECT 
+		DATE_FORMAT(re.Fecha, '%d/%m/%Y') AS Fecha,
+		re.id_trab,
+		CONCAT_WS(
+		  ' ',
+		  tr.apepat_trab,
+		  tr.apemat_trab,
+		  tr.nom_trab
+		) AS nombres,
+		tsua.des_larga AS sucursal_anexo,
+		tfun.des_larga AS funcion,
+		tare.des_larga AS area_trab,
+		tr.est_reg,
+		tr.num_doc_trab,
+		re.hor_ent,
+		re.hor_sal,
+		fe.estado,
+		'' AS detalle 
+	  FROM
+		reloj re 
+		INNER JOIN trabajador tr 
+		  ON re.id_trab = tr.id_trab 
+		LEFT JOIN contratos co 
+		  ON tr.id_trab = co.id_trab 
+		LEFT JOIN tabla_maestra_detalle AS tsua 
+		  ON tsua.cod_argumento = tr.id_sucursal 
+		  AND tsua.cod_tabla = 'TSUA' 
+		LEFT JOIN tabla_maestra_detalle AS tfun 
+		  ON tfun.cod_argumento = tr.id_funcion 
+		  AND tfun.cod_tabla = 'TFUN' 
+		LEFT JOIN tabla_maestra_detalle AS tare 
+		  ON tare.cod_argumento = tr.id_area 
+		  AND tare.cod_tabla = 'TARE' 
+		LEFT JOIN fechas fe 
+		  ON fe.fecha = re.fecha 
+	  WHERE tr.`est_reg` = '1' 
+	  	AND re.id_trab LIKE '%$id_trab%'
+		AND DATE(re.fecha)>'$fecha_inicio' AND DATE(re.fecha)<'$fecha_fin'
+	  ORDER BY re.fecha DESC";
+
+		return ejecutarConsulta($sql);		
+	}
+
+	public function ventacabecera($idventa){
+		$sql="SELECT v.idventa,v.idcliente,p.nombre as cliente,p.direccion,p.tipo_documento,p.num_documento,p.email,p.telefono,v.idusuario,u.nombre as usuario,v.tipo_comprobante,v.serie_comprobante,v.num_comprobante,date(v.fecha_hora) as fecha,v.impuesto,v.total_venta FROM venta v INNER JOIN persona p ON v.idcliente=p.idpersona INNER JOIN usuario u ON v.idusuario=u.idusuario WHERE v.idventa='$idventa'";
+		return ejecutarConsulta($sql);
+	}
+
+	public function ventadetalle($idventa){
+		$sql="SELECT a.nombre as articulo,a.codigo,d.cantidad,d.precio_venta,d.descuento,(d.cantidad*d.precio_venta-d.descuento) as subtotal FROM detalle_venta d INNER JOIN articulo a ON d.idarticulo=a.idarticulo WHERE d.idventa='$idventa'";
+		return ejecutarConsulta($sql);
+	}
+
+	//Implementar un método para listar los registros
+	public function obtenercantidaditems($nro_doc)
+	{
+		$sql="SELECT MAX(correlativo)
+		 FROM vacaciones 
+		 WHERE nro_doc='$nro_doc' 
+		 ";
+		return ejecutarConsulta($sql);	
+	}
+
+
+
+	
+}
+?>
---
+++ vistas/registro_marcaciones.php
@@ -0,0 +1,109 @@
+<?php
+//Activamos el almacenamiento en el buffer
+ob_start();
+session_start();
+
+if (!isset($_SESSION["nombre"]))
+{
+  header("Location: login.html");
+}
+else
+{
+require 'header.php';
+
+if ($_SESSION['rrhh']==1)
+{
+?>
+<!--Contenido-->
+      <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+          <!-- Main content -->
+          <section class="content">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="box">
+                  <div class="box-header with-border">
+                    <h1 class="box-title">Registro de Marcaciones</h1>
+                    <a href="../reportes/rpt_xls_contratos.php" target="_blank"><button class="btn btn-info">Reporte</button></a>
+                    </h1>
+                    <div class="box-tools pull-right">
+                    </div>
+                  </div>
+                  <!-- /.box-header -->
+                  <!-- centro -->
+                  <div class="panel-body table-responsive" id="listadoregistros">
+
+                    <div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                      <label>Fecha Inicio</label>
+                      <input type="date" class="form-control" name="fecha_inicio" id="fecha_inicio" value="<?php echo date("Y-m-d"); ?>">
+                    </div>
+                    <div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                      <label>Fecha Fin</label>
+                      <input type="date" class="form-control" name="fecha_fin" id="fecha_fin" value="<?php echo date("Y-m-d"); ?>">
+                    </div>
+                    <div class="form-inline col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                      <label>Trabajador</label>
+                      <select name="id_trab" id="id_trab" class="form-control selectpicker" data-live-search="true"
+                        required>
+                      </select>
+                      <button class="btn btn-success" onclick="listar()">Mostrar</button>
+                    </div>
+
+                    <table id="tbllistado" class="table table-striped table-bordered table-condensed table-hover">
+                      <thead>
+
+                        <th>Dia</th>
+                        <th>Estado de Dia</th>
+                        <th>Sucursal</th>
+                        <th>Trabajador</th>
+                        <th>Area</th>
+                        <th>Hora Ingreso</th>
+                        <th>Hora Salida</th>
+                        <th>Detalle</th>
+
+                      </thead>
+                      <tbody>
+                      </tbody>
+                      <tfoot>
+
+                        <th>Dia</th>
+                        <th>Estado de Dia</th>
+                        <th>Sucursal</th>
+                        <th>Trabajador</th>
+                        <th>Area</th>
+                        <th>Hora Ingreso</th>
+                        <th>Hora Salida</th>
+                        <th>Detalle</th>
+                      </tfoot>
+                    </table>
+                  </div>
+
+
+
+                  <!--Fin centro -->
+                </div><!-- /.box -->
+              </div><!-- /.col -->
+            </div><!-- /.row -->
+          </section><!-- /.content -->
+
+        </div><!-- /.content-wrapper -->
+  <!--Fin-Contenido-->
+
+  
+
+<?php
+}
+else
+{
+  require 'noacceso.php';
+}
+
+require 'footer.php';
+?>
+<script type="text/javascript" src="scripts/registro_marcaciones.js"></script>
+<?php 
+}
+ob_end_flush();
+?>
+
+
